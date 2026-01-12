cad245a3be8a9d435974ef521db06c82b614576d
Ishaan Bhalerao <ibhalerao188@student.fuhsd.org>
2026-01-11 19:08:54 -0800

    Filled Money with 3 methods, hardest might be checking tho

--- src/properties/Money.java
+++ src/properties/Money.java
@@ -1,5 +1,53 @@
 package properties;
+import pets.Pet;
+
 
 public class Money {
+	
+	private String[] availableToys;     
+	private double[] toyPrices;         
+
+	private String[] availableFoods;     
+	private double[] foodPrices;         
+	
+	public Money() {
+		availableToys = new String[]{"Used ball", "A Rubber Chew Bone", "Guardian Plus Bear", "Enchanted Chew Wand", "Ecstatic Frizzby", "Golden Bone", 
+				"Drift Ball", "Shell Hideout", "Bioluminescent Bubble Machine", "Robotic Friend", "Ancient Calming Bell", "Platinum Speaker For Music",
+				"Plain Sock", "Jingle Mouse", "Corner Scratch Post", "Laser Pointer", "Quarter Zip", "Legendary Airpods"};
+		toyPrices = new double[] {3.00, 4.55, 10.00, 18.00, 40.00, 70.00,
+				2.55, 11.00, 15.00, 35.50, 75.00, 100.00, 
+				3.00, 6.00, 15.00, 30.00, 50.00, 120.00};
+		
+		availableFoods = new String[] {"Kibble Crunch", "Beef & Veggie Mix", "Chicken Feast", "Salmon Delight", "Lamb & Rice Bowl", "Golden Chicken Nuggets",
+				"Flake Feast", "Algae Crisps", "Shrimp Pellets", "Spirulina Flakes", "Caviar Pearls", "Deep Abyss Infusion",
+				"Mixed Kibble Mix", "Veggie Medley", "Turkey & Cranberry Treats", "Expensive Milk","Crunchy Bluefin Tuna Sushi", "One of a kind Lamb Delight" };
+		foodPrices = new double[] {10.00};
+		
+		
+		
+	}
+	
+	
+	public boolean checkIfSelec() {
+		return false;
+		
+	}
+	
+	public void buyFood(Food f, Pet p) {
+		double m = p.getMoney();
+		
+		
+		
+		
+	}
+	
+	public void buyToys(Pet p) {
+		double m = p.getMoney();
+		
+		
+	}
+	
+	
+	
 
 }
