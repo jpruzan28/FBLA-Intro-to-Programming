c3dfd8a8c348c5d6e907642d65fe3d7840fb1242
elo989 <elo989@student.fuhsd.org>
2026-02-03 22:43:01 -0800

    did timer adding

--- src/DrawingSurface/ReductionTimer.java
+++ src/DrawingSurface/ReductionTimer.java
@@ -1,7 +1,27 @@
 package DrawingSurface;
 
+import pets.Pet;
 import java.util.TimerTask;
 
 public class ReductionTimer {
-
+	
+	Pet pet;
+	int ogHunger;
+	int ogHealth;
+	int ogEmotion;
+	int ogRest;
+	int ogHygiene;
+	
+	public ReductionTimer(Pet pet) {
+		this.pet = pet;
+		ogHunger = pet.getHunger();
+		ogHealth = pet.getHealth();
+		ogEmotion = pet.getEmotion();
+		ogRest = pet.getRest();
+		ogHygiene = pet.getHygiene();
+		
+		pet.setHunger(ogHunger--);
+		
+		
+	}
 }
