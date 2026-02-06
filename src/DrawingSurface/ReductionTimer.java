ce93ad29354773532b017df8d8dbf4b691dc0360
elo989 <elo989@student.fuhsd.org>
2026-02-05 22:18:22 -0800

    Finished Reduction Timer

--- src/DrawingSurface/ReductionTimer.java
+++ src/DrawingSurface/ReductionTimer.java
@@ -3,7 +3,7 @@ package DrawingSurface;
 import pets.Pet;
 import java.util.TimerTask;
 
-public class ReductionTimer extends TimerTask{
+public class ReductionTimer{
 	
 	Pet pet;
 	int ogHunger;
@@ -19,10 +19,51 @@ public class ReductionTimer extends TimerTask{
 		ogEmotion = pet.getEmotion();
 		ogRest = pet.getRest();
 		ogHygiene = pet.getHygiene();
-		
-		pet.setHunger(ogHunger--);
-		
-	} public void reduceHunger() {
-		
 	}
-}
+		
+
+	 public class reduceHunger extends TimerTask { 
+		 @Override
+		public void run() {
+			 if (ogHunger>=0) {
+			pet.setHunger(ogHunger--);
+			System.out.println(ogHunger);
+			 }
+		}
+		}
+	 
+	 public class reduceHealth extends TimerTask { 
+		 @Override
+		public void run() {
+			 if (ogHealth>=0) {
+			pet.setHealth(ogHealth--);
+			 }
+		}
+		}
+	 public class reduceHygiene extends TimerTask { 
+		 @Override
+		public void run() {
+			 if (ogHygiene>=0) {
+			pet.setHealth(ogHygiene--);
+			 }
+		}
+		}
+	 public class reduceEmotion extends TimerTask { 
+		 @Override
+		public void run() {
+			 if (ogEmotion>=0) {
+			pet.setHealth(ogEmotion--);
+			 }
+		}
+		}	
+
+		public class reduceRest extends TimerTask {
+			@Override
+			public void run() {
+				if (ogHunger >= 0) {
+					pet.setHealth(ogEmotion--);
+				}
+			}
+		}
+	}
+
