6b613f18b23afd33310178bf2f41b7ecc0c35011
elo989 <elo989@student.fuhsd.org>
2026-02-08 18:51:24 -0800

    added sprites but need help

--- src/DrawingSurface/Sprite.java
+++ src/DrawingSurface/Sprite.java
@@ -6,15 +6,26 @@ import java.awt.Image;
 import java.awt.Container;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+
+import pets.Pet;
+
 import javax.swing.ImageIcon;
 
 public class Sprite extends JPanel{
+	
+	private Pet pet;
+	private String petType;
+	
 	private Image cat1, cat2, cat3, dog1, dog2, dog3, fish1, fish2, fish3;
 	private Image happyCat, unhappyCat, neutralCat;
 	private Image happyDog, unhappyDog, neutralDog;
 	private Image happyFish, unhappyFish, neutralFish;
 	
-	public Sprite() {
+	public Sprite(Pet pet) {
+		
+		this.pet = pet;
+		petType = pet.getType();
+		
 		cat3 = (new ImageIcon("cat.unhappy.jpg")).getImage();
 		cat2 = (new ImageIcon("cat.happy.jpg")).getImage();
 		cat1 = (new ImageIcon("cat.neutral.jpg")).getImage();
@@ -41,6 +52,14 @@ public class Sprite extends JPanel{
 	  {
 //	    super.paintComponent(g); good for something idk
 //if statements here
-		 g.drawImage(happyCat, 0, 0, null);
+		 if (petType.equalsIgnoreCase("cat")) {
+		 g.drawImage(neutralCat, 0, 0, null);
+		 }
+		 else if (petType.equalsIgnoreCase("dog")) {
+		 g.drawImage(neutralDog, 0, 0, null);
+		 }
+		 else if (petType.equalsIgnoreCase("fish")) {
+		 g.drawImage(neutralFish, 0, 0, null);
+		 }
 	  }
 }
