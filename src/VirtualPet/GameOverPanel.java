561d6c67bbef55b9b866fa3dda8916fc4cbff1cb
Ishaan Bhalerao <ibhalerao188@student.fuhsd.org>
2026-03-29 12:11:48 -0700

    read me file

--- src/VirtualPet/GameOverPanel.java
+++ src/VirtualPet/GameOverPanel.java
@@ -16,7 +16,7 @@ public class GameOverPanel extends JPanel {
 
     public GameOverPanel(CardLayout cardLayout, JPanel cardPanel, Pet pet) {
         setLayout(new BorderLayout());
-        setBackground(Color.cyan);
+        setBackground(Color.orange);
 
         // Title
         JLabel titleLabel = new JLabel("GAME OVER! :<", SwingConstants.CENTER);
@@ -36,17 +36,18 @@ public class GameOverPanel extends JPanel {
 
         // 3. Happiness based on emotion
         String happiness;
-        if(pet.getEmotion() >= 8) {happiness = "Very Happy :)";}
-        else if (pet.getEmotion() >= 5) {happiness = "Content :|";}
+        if(pet.getEmotion() >= 8) {happiness = "Very Happy :) Amazing job!";}
+        else if (pet.getEmotion() >= 5) {happiness = "Content :| Nice preventing your pet from feeling sad ";}
         
-        else if (pet.getEmotion() >= 3) {happiness = "Sad :(";}
+        else if (pet.getEmotion() >= 3) {happiness = "Sad :( It's okay though you tried your best";}
+        
         else {happiness = "Very Unhappy >:[";}
-        JLabel happinessLabel = new JLabel("Happiness: " + happiness, SwingConstants.CENTER);
+        JLabel happinessLabel = new JLabel("During your run, the pet felt " + happiness, SwingConstants.CENTER);
 
         // Set font for all labels
         Font statFont = new Font("Arial", Font.BOLD, 28);
-        healthLabel  .setFont(statFont);
-        hungerLabel  .setFont(statFont);
+        healthLabel.setFont(statFont);
+        hungerLabel.setFont(statFont);
         happinessLabel.setFont(statFont);
 
         // Add to stats panel
