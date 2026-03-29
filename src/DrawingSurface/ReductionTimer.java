023828233dae755264ec91ed471040cd3651c6c5
Ishaan Bhalerao <ibhalerao188@student.fuhsd.org>
2026-03-29 12:55:12 -0700

    got some help with fixing reduction timer

--- src/DrawingSurface/ReductionTimer.java
+++ src/DrawingSurface/ReductionTimer.java
@@ -5,97 +5,79 @@ import properties.*;
 import java.util.TimerTask;
 
 public class ReductionTimer {
-	
-	Pet pet;
-	Emotion emotionC;
-	private DrawingSurface drawingSurface;
-	private int ogHunger;
-	private int ogHealth;
-	private int ogEmotion;
-	private int ogRest;
-	int ogHygiene;
-	
-	public ReductionTimer(Pet pet, DrawingSurface drawingSurface) {
-		this.pet = pet;
-		this.drawingSurface = drawingSurface;
-		
-		emotionC = new Emotion(pet);
-		
-		ogHunger = pet.getHunger();
-		ogHealth = pet.getHealth();
-		ogEmotion = pet.getEmotion();
-		ogHygiene = pet.getHygiene();
-		ogRest = pet.getRest();
-	}
 
-	public class reduceHunger extends TimerTask {
-		@Override
-		public void run() {
-			if (ogHunger >= 0) {
-				pet.setHunger(ogHunger--);
-			}
-		}
-	}
+    Pet pet;
+    Emotion emotionC;
+    private DrawingSurface drawingSurface;
 
-	public class reduceHygiene extends TimerTask {
-		@Override
-		public void run() {
-			if (ogHygiene >= 0) {
-				pet.setHygiene(ogHygiene--);
-			}
+    public ReductionTimer(Pet pet, DrawingSurface drawingSurface) {
+        this.pet = pet;
+        this.drawingSurface = drawingSurface;
+        emotionC = new Emotion(pet);
+    }
 
-		}
-	}
+    public class reduceHunger extends TimerTask {
+        @Override
+        public void run() {
+            int current = pet.getHunger();
+            if (current > 0) {
+                pet.setHunger(current - 1);
+            }
+        }
+    }
 
-	public class reduceEmotion extends TimerTask {
-		@Override
-		public void run() {
-			if (ogEmotion >= 0) {
-				pet.setEmotion(ogEmotion--);
-			}
-			emotionC.calculatingEmotion();
-		}
-	}
+    public class reduceHygiene extends TimerTask {
+        @Override
+        public void run() {
+            int current = pet.getHygiene();
+            if (current > 0) {
+                pet.setHygiene(current - 1);
+            }
+        }
+    }
 
-	public class reduceRest extends TimerTask {
-		@Override
-		public void run() {
-			if (ogRest >= 0) {
-				pet.setRest(ogRest--);
-			}
-			
-		}
-	}
+    public class reduceEmotion extends TimerTask {
+        @Override
+        public void run() {
+            int current = pet.getEmotion();
+            if (current > 0) {
+                pet.setEmotion(current - 1);
+            }
+            emotionC.calculatingEmotion();
+        }
+    }
 
-//	public class salary extends TimerTask {
-//		@Override
-//		public void run() {
-//			pet.setMoney(pet.getMoney() + 10);
-//			drawingSurface.updateMoneyDisplay();
-//		}
-//	}
+    public class reduceRest extends TimerTask {
+        @Override
+        public void run() {
+            int current = pet.getRest();
+            if (current > 0) {
+                pet.setRest(current - 1);
+            }
+        }
+    }
 
-	public class bonus extends TimerTask {
-		@Override
-		public void run() {
-			if (pet.getHealth() == 10) {
-				pet.setMoney(pet.getMoney() + 5);
-			}
-			if (pet.getEmotion() == 10) {
-				pet.setMoney(pet.getMoney() + 5);
-			}
-			drawingSurface.updateMoneyDisplay();
-		}
-	}
+    public class bonus extends TimerTask {
+        @Override
+        public void run() {
+            if (pet.getHealth() == 10) {
+                pet.setMoney(pet.getMoney() + 5);
+            }
+            if (pet.getEmotion() == 10) {
+                pet.setMoney(pet.getMoney() + 5);
+            }
+            drawingSurface.updateMoneyDisplay();
+        }
+    }
 
-	public class updateHealth extends TimerTask {
-		@Override
-		public void run() {
-			Health petHealth = new Health(pet);
-			petHealth.addHungerToHealth(pet.getHunger());
-			petHealth.addRestToHealth(pet.getRest());
-			petHealth.addHygieneToHealth(pet.getHygiene());
-			petHealth.remindVetVisit();
-		}
-	}
+    public class updateHealth extends TimerTask {
+        @Override
+        public void run() {
+            Health petHealth = new Health(pet);
+            petHealth.addHungerToHealth(pet.getHunger());
+            petHealth.addRestToHealth(pet.getRest());
+            petHealth.addHygieneToHealth(pet.getHygiene());
+            petHealth.remindVetVisit();
+        }
+    }
 }
