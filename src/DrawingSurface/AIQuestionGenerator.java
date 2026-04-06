a83681368f513515243a73559a60a42c373033b7
Jasmine Pruzan <jasmine.jazzy.pruzan@gmail.com>
2026-04-05 19:05:06 -0700

    Changed file path for ai

--- src/DrawingSurface/AIQuestionGenerator.java
+++ src/DrawingSurface/AIQuestionGenerator.java
@@ -16,8 +16,9 @@ public class AIQuestionGenerator {
 	public static String loadApiKey() {
 	    try {
 	        Properties props = new Properties();
-	        props.load(new FileInputStream("C:/Users/jasmi/Documents/GitHub/FBLA-Intro-to-Programming/src/DrawingSurface/config.properties"));	        
-	        
+	        String path = System.getProperty("user.home") + "/Documents/GitHub/FBLA-Intro-to-Programming/src/DrawingSurface/config.properties";
+	        props.load(new FileInputStream(path));	       
+
 	        return props.getProperty("APIKEY");
 	    } catch (Exception e) {
 	        e.printStackTrace();
