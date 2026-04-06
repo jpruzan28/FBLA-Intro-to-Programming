b252d0dec94e3c117f57ed7670d7759e517ea5ca
elo989 <elo989@student.fuhsd.org>
2026-04-05 17:51:30 -0700

    Updated buttons in storepanel

--- src/VirtualPet/StorePanel.java
+++ src/VirtualPet/StorePanel.java
@@ -9,11 +9,11 @@ import pets.Pet;
 import java.awt.*;
 import java.awt.event.*;
 
-public class StorePanel extends JPanel {
+public class StorePanel extends JPanel implements ActionListener{
 	
-	JButton usedBall, rubberChewBone, enchantedChewWand, guardianPlusBear, dogGodWorshipAltar, goldenestBone;
-    JButton driftBall, shellHideout, biolumeBubbleMachine, roboticFriend, ancientCalmingBell, platinumSpeaker;
-	JButton plainKittySock, jingleMouse, cornerScratchPost, laserPointer, quarterZip, legendaryAirpods;
+	JButton usedBall, enchantedChewWand, goldenestBone;
+    JButton driftBall, biolumeBubbleMachine, platinumSpeaker;
+	JButton plainKittySock, laserPointer, legendaryAirpods;
 
 	Pet p;
 	private BackgroundPanel backgroundPanel;
@@ -37,103 +37,61 @@ public class StorePanel extends JPanel {
     	add(allToysPanel, BorderLayout.CENTER);
     	addBackground();
 
-                // Add store title
-                
-//        // Create jbuttons
-        usedBall = new JButton("Used Ball: $4.00");  
+    	
+        // Naming buttons
+    	usedBall = new JButton("Used Ball: $4.00");  
         enchantedChewWand = new JButton("Enchanted Chew Wand: $25.00"); 
         goldenestBone = new JButton("Goldenest Bone: $90.00"); 
-//        
-//        // Names buttons
+        plainKittySock = new JButton("Plain Kitty Sock: $4.00");
+        laserPointer = new JButton("Laser Pointer: $50.00");
+        legendaryAirpods = new JButton("Legendary Airpods: $90.00");
+        driftBall = new JButton("Drift Ball: $4.00"); 
+        biolumeBubbleMachine = new JButton("Biolume Bubble Machine: $25.00"); 
+        platinumSpeaker = new JButton("Platinum Speaker: $90.00"); 
+        
+        //Setting button location
+        usedBall.setBounds(90,  460, 120, 40);
+        enchantedChewWand.setBounds(90, 100, 120, 40);
+        goldenestBone.setBounds(90, 160, 120, 40);
+        laserPointer.setBounds(90, 220, 120, 40);
+        plainKittySock.setBounds(90, 280, 120, 40);
+        legendaryAirpods.setBounds(90,  340, 120, 40);
+        driftBall.setBounds(90,  400, 120, 40);
+        
+        plainKittySock.setName("Plain Kitty Sock");
+        laserPointer.setName("Laser Pointer");
+        legendaryAirpods.setName("Legendary Airpods");
+        driftBall.setName("Drift Ball"); 
+        biolumeBubbleMachine.setName("Biolume Bubble Machine"); 
+        platinumSpeaker.setName("Platinum Speaker"); 
         usedBall.setName("Used Ball"); 
         enchantedChewWand.setName("Enchanted Chew Wand");  
-        goldenestBone.setName("Goldenest Bone"); 
-     
-        // Track when buttons are clicked
+        goldenestBone.setName("Goldenest Bone");
+        
+                // Track when buttons are clicked
         usedBall.addActionListener(this);
         enchantedChewWand.addActionListener(this);
-        goldenestBone.addActionListener(this);  
+        goldenestBone.addActionListener(this); 
+        driftBall.addActionListener(this);
+        biolumeBubbleMachine.addActionListener(this);
+        platinumSpeaker.addActionListener(this);
+        plainKittySock.addActionListener(this);
+        laserPointer.addActionListener(this);
+        legendaryAirpods.addActionListener(this);
         
-//        // Adds buttons to screen
+        
+        // Add buttons to screen 
         layeredPane.add(usedBall,  JLayeredPane.PALETTE_LAYER);
         layeredPane.add(enchantedChewWand,  JLayeredPane.PALETTE_LAYER);
         layeredPane.add(goldenestBone,  JLayeredPane.PALETTE_LAYER);
-   
+        layeredPane.add(plainKittySock, JLayeredPane.PALETTE_LAYER);
+        layeredPane.add(laserPointer, JLayeredPane.PALETTE_LAYER);
+        layeredPane.add(legendaryAirpods, JLayeredPane.PALETTE_LAYER);
+        layeredPane.add(driftBall, JLayeredPane.PALETTE_LAYER);
+        layeredPane.add(biolumeBubbleMachine, JLayeredPane.PALETTE_LAYER);
+        layeredPane.add(platinumSpeaker, JLayeredPane.PALETTE_LAYER);
+        
 
-//
-//        
-//        // Creates panel
-//        JPanel fishToysPanel = new JPanel();
-//        fishToysPanel.setLayout(new GridLayout(3, 2, 8, 8));
-//        fishToysPanel.setBackground(Color.RED);
-//       
-//        // Create jbuttons
-//        driftBall = new JButton("Drift Ball: $4.00"); 
-//        shellHideout = new JButton("Shell Hideout: $8.00"); 
-//        biolumeBubbleMachine = new JButton("Biolume Bubble Machine: $25.00"); 
-//        roboticFriend = new JButton("Robotic Friend: $50.00"); 
-//        ancientCalmingBell = new JButton("Ancient Calming Bell: $70.00"); 
-//        platinumSpeaker = new JButton("Platinum Speaker: $90.00"); 
-//        
-//        // Naming buttons
-//        driftBall.setName("Drift Ball"); 
-//        shellHideout.setName("Shell Hideout"); 
-//        biolumeBubbleMachine.setName("Biolume Bubble Machine"); 
-//        roboticFriend.setName("Robotic Friend"); 
-//        ancientCalmingBell.setName("Ancient Calming Bell"); 
-//        platinumSpeaker.setName("Platinum Speaker"); 
-//
-//        // Adding buttons to screen
-//        fishToysPanel.add(driftBall);
-//        fishToysPanel.add(shellHideout);
-//        fishToysPanel.add(biolumeBubbleMachine);
-//        fishToysPanel.add(roboticFriend);
-//        fishToysPanel.add(ancientCalmingBell);
-//        fishToysPanel.add(platinumSpeaker);
-//        
-//        driftBall.addActionListener(this);
-//        shellHideout.addActionListener(this);
-//        biolumeBubbleMachine.addActionListener(this);
-//        roboticFriend.addActionListener(this);
-//        ancientCalmingBell.addActionListener(this);
-//        platinumSpeaker.addActionListener(this);
-//
-//
-//        // Create jpanel
-//        JPanel catToysPanel = new JPanel();
-//        catToysPanel.setLayout(new GridLayout(3, 2, 8, 8));
-//        catToysPanel.setBackground(Color.BLUE);
-//       
-//        // Naming buttons
-//        plainKittySock = new JButton("Plain Kitty Sock: $4.00");
-//        jingleMouse = new JButton("Jingle Mouse: $8.00");
-//        cornerScratchPost = new JButton("Corner Scratch Post: $25.00");
-//        laserPointer = new JButton("Laser Pointer: $50.00");
-//        quarterZip = new JButton("Quarter Zip: $70.00");
-//        legendaryAirpods = new JButton("Legendary Airpods: $90.00");
-//        
-//        // Create jbuttons
-//        plainKittySock.setName("Plain Kitty Sock");
-//        jingleMouse.setName("Jingle Mouse");
-//        cornerScratchPost.setName("Corner Scratch Post");
-//        laserPointer.setName("Laser Pointer");
-//        quarterZip.setName("Quarter Zip");
-//        legendaryAirpods.setName("Legendary Airpods");
-//        
-//        // Add buttons to screen
-//        catToysPanel.add(plainKittySock);
-//        catToysPanel.add(jingleMouse);
-//        catToysPanel.add(cornerScratchPost);
-//        catToysPanel.add(laserPointer);
-//        catToysPanel.add(quarterZip);
-//        catToysPanel.add(legendaryAirpods);
-//        
-//        plainKittySock.addActionListener(this);
-//        jingleMouse.addActionListener(this);
-//        cornerScratchPost.addActionListener(this);
-//        laserPointer.addActionListener(this);
-//        quarterZip.addActionListener(this);
-//        legendaryAirpods.addActionListener(this);
 
 
 
@@ -143,12 +101,7 @@ public class StorePanel extends JPanel {
         });
         add(backButton, BorderLayout.SOUTH); 
         
-        
-        //We add the dog, fish, and cat panels the all toys panel
-        //the all toys panel is a panel with 3 different panels
-//        allToysPanel.add(dogToysPanel);
-//        allToysPanel.add(fishToysPanel);
-//        allToysPanel.add(catToysPanel);
+       
         
         
         
@@ -193,42 +146,42 @@ public class StorePanel extends JPanel {
         }
         
         
-//        JButton[] fishToys = new JButton[] {driftBall, shellHideout, biolumeBubbleMachine, roboticFriend, ancientCalmingBell, platinumSpeaker};
-//
-//        // Goes through JButton array
-//        for(JButton t: fishToys) {
-//        	// Checks if the button clicked is the same as the JButton
-//        	if (button.equals(t)) {
-//        		// Goes through Toys array from Money class
-//        		for(Toys y: store.getFishToys()) {
-//        			// Checks if the name of the toy matches the JButton name
-//        			if(t.getName().equals(y.getName())) {
-//        				store.buyToys(y, p);
-//        			}
-//        		}
-//        	}
-//        }
-//        
-//        
-//        JButton[] catToys = new JButton[] {plainKittySock, jingleMouse, cornerScratchPost, laserPointer, quarterZip, legendaryAirpods};
-//
-//        
-//        // Goes through JButton array
-//        for(JButton t: catToys) {
-//        	// Checks if the button clicked is the same as the JButton
-//        	if (button.equals(t)) {
-//        		// Goes through Toys array from Money class
-//        		for(Toys y: store.getCatToys()) {
-//        			// Checks if the name of the toy matches the JButton name
-//        			if(t.getName().equals(y.getName())) {
-//        				store.buyToys(y, p);
-//        			}
-//        		}
-//        	}
-//        }
-//
-//    
-//    }
-//    
-//    
+        JButton[] fishToys = new JButton[] {driftBall, biolumeBubbleMachine, platinumSpeaker};
+
+        // Goes through JButton array
+        for(JButton t: fishToys) {
+        	// Checks if the button clicked is the same as the JButton
+        	if (button.equals(t)) {
+        		// Goes through Toys array from Money class
+        		for(Toys y: store.getFishToys()) {
+        			// Checks if the name of the toy matches the JButton name
+        			if(t.getName().equals(y.getName())) {
+        				store.buyToys(y, p);
+        			}
+        		}
+        	}
+        }
+        
+        
+        JButton[] catToys = new JButton[] {plainKittySock, laserPointer, legendaryAirpods};
+
+        
+        // Goes through JButton array
+        for(JButton t: catToys) {
+        	// Checks if the button clicked is the same as the JButton
+        	if (button.equals(t)) {
+        		// Goes through Toys array from Money class
+        		for(Toys y: store.getCatToys()) {
+        			// Checks if the name of the toy matches the JButton name
+        			if(t.getName().equals(y.getName())) {
+        				store.buyToys(y, p);
+        			}
+        		}
+        	}
+        }
+
+    
+    }
+    
+    
 }    
